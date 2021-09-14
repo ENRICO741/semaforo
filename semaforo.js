d4d787ee2b5aafb2156cffa66e4b4c9e804ba7b3
ENRICO MARÇOLA FEITOZA <21193365@CFP127ALUNO.LOCAL>
2021-09-14 16:29:26 -0300

    finalizado

--- semaforo.js
+++ semaforo.js
@@ -1,19 +1,23 @@
 const semaforo = document.getElementById("semaforo")
+const auto = document.getElementById("auto")
 let idInterval
 
 function amarelo() {
     semaforo.src = "img/amarelo.png" 
     pararPiscar()
+    auto.textContent = "Automático"
 }
 
 function verde() {
     semaforo.src = "img/verde.png" 
     pararPiscar()
+    auto.textContent = "Automático"
 }
 
 function vermelho() {
     semaforo.src = "img/vermelho.png" 
     pararPiscar()
+    auto.textContent = "Automático"
 }
 
 function desligada() {
@@ -33,12 +37,21 @@ function trocarImagem() {
     }
     else if(semaforo.src.includes("amarelo")){
         semaforo.src = "img/verde.png" 
-    } 
+    }
+    else if(semaforo.src.includes("verde")){
+        semaforo.src = "img/desligado.png"
+    }
 }
 
-function auto() {
-    const auto = document.getElementById("auto")
+function autoCor(){
+    if(auto.textContent == "Automático"){
     idInterval = setInterval(trocarImagem, 1000)
+        auto.textContent = "Parar"
+    } 
+    else {
+        pararPiscar()
+        auto.textContent = "Automático"
+    }
 }
 
 document.getElementById("amarelo")
@@ -51,7 +64,7 @@ document.getElementById("vermelho")
     .addEventListener("click", vermelho)
 
 document.getElementById("auto")
-    .addEventListener("click", auto)
+    .addEventListener("click", autoCor)
 
 
 
